ec14b5643772e5df1785fc46b58a1e35a05643c5
mmm2404 <106526980+mmm2404@users.noreply.github.com>
2023-04-14 22:25:02 +0300

    Add files via upload

---
+++ src/mockdata.js
@@ -0,0 +1,28 @@
+ const backLogList = [
+    {
+      id: 1,
+      name: "Login page – performance issues",
+      description: "Optimize the logging function",
+      created: new Date().toString(),
+    },
+    {
+      id: 2,
+      name: "Sprint bugfix",
+      description: "Fix all the bugs",
+      created: new Date().toString(),
+    },
+    {
+      id: 3,
+      name: "Add user section to settings",
+      description: "There should be a user section in setting where users can edit their profile data: name, phone, email, etc.",
+      created: new Date().toString(),
+    },
+    {
+      id: 4,
+      name: "Implement loader",
+      description: "Fetching data from server takes some time. We need to implement a loader so user can see that data fetching is in progress.",
+      created: new Date().toString(),
+    },
+  ];
+
+  export default backLogList;
